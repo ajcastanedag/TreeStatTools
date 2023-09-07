3a0e41358b46f3703e3b219d611064699352c362
ajcastanedag <antonio.castanedag@gmail.com>
2023-09-07 23:02:39 +0200

    Code optimization by preallocationg memory

--- TreeCrown.R
+++ TreeCrown.R
@@ -2,7 +2,7 @@
 pacman::p_load(dplyr,sf,ggplot2, mapview, st, units, REdaS)
 
 # Set WD
-Root <- "/home/cowboybebop/Documents/PhD/Codes/TreeDiam/" %>% setwd()
+Root <- "" %>% setwd()
 
 # Import GPKG 
 Trees <- st_read("TreeSample.gpkg")
@@ -10,36 +10,36 @@ Trees <- st_read("TreeSample.gpkg")
 # Create centroids
 centroids <- st_centroid(Trees) 
 
-# Create additional fields for the data storage
-centroids$min <- NA
-centroids$med <- NA
-centroids$mean <- NA
-centroids$max <- NA
-
 # Count total of shapes
 TotalTrees <- length(centroids[[1]])
 
-# CReate progress bar
+# Precompute centroid coordinates
+centroid_coords <- st_coordinates(centroids)
+
+# Initialize vectors to store results
+min_lengths <- numeric(TotalTrees)
+med_lengths <- numeric(TotalTrees)
+mean_lengths <- numeric(TotalTrees)
+max_lengths <- numeric(TotalTrees)
+
+# Create progress bar
 pb <- txtProgressBar(min = 1, max = TotalTrees, style = 3)
 
 # Loop over all available tree shapes
-for(i in 1:TotalTrees){
+for (i in 1:TotalTrees) {
+  
   # Convert polygon into points
-  PolPoints <- st_cast(Trees[i,],"POINT")
+  PolPoints <- st_cast(Trees[i,], "POINT")
   
-  # Create rotated line object
-  combined_lines <- st_sfc(geometry = st_sfc()) %>% st_set_crs(st_crs(centroids))
+  # Initialize variables to store lengths
+  Lengthvector <- numeric(length(PolPoints[[1]]))
   
-  # Create a new vector to store lengths
-  Lengthvector <- vector()
-  
-  # Loop over all poligon points to create the lines between centroid and polygon edges
-  for(j in 1:length(PolPoints[[1]])){
-    
+  # Loop over all polygon points to create the lines between centroid and polygon edges
+  for (j in 1:length(PolPoints[[1]])) {
     # Define a set of coordinates to create line from the two points
     coordinates <- matrix(c(
-      c(st_coordinates(PolPoints[j,])[1], st_coordinates(centroids[i,])[1]),
-      c(st_coordinates(PolPoints[j,])[2], st_coordinates(centroids[i,])[2])
+      st_coordinates(PolPoints[j,][1])[1], centroid_coords[i,][1],
+      st_coordinates(PolPoints[j,][1])[2], centroid_coords[i,][2]
     ), ncol = 2)
     
     # Create an sf LineString object
@@ -48,19 +48,15 @@ for(i in 1:TotalTrees){
     # Calculate length of line
     Length <- st_length(line)
     
-    # Combine the rotated LINESTRING objects into a single sfc object
-    combined_lines <- st_combine(c(combined_lines, line))
-    
-    # Add up the lengths in the vector
-    Lengthvector <- c(Lengthvector,c(Length))
-    
+    # Store the length in the vector
+    Lengthvector[j] <- Length
   }
   
-  # Calculate fields from the lines and store them on the respective centroid 
-  centroids$min[i] <- round(min(Lengthvector),2)
-  centroids$med[i] <- round(median(Lengthvector),2)
-  centroids$mean[i] <- round(mean(Lengthvector),2)
-  centroids$max[i] <- round(max(Lengthvector),2)
+  # Calculate fields from the lines and store them in vectors
+  min_lengths[i] <- round(min(Lengthvector), 2)
+  med_lengths[i] <- round(median(Lengthvector), 2)
+  mean_lengths[i] <- round(mean(Lengthvector), 2)
+  max_lengths[i] <- round(max(Lengthvector), 2)
   
   # Increment progress bar
   setTxtProgressBar(pb, i)
@@ -69,13 +65,73 @@ for(i in 1:TotalTrees){
   if(i == TotalTrees){
     close(pb)
   }
-  
 }
+
+# Assign the vectors to the centroids data frame
+centroids$min <- min_lengths
+centroids$med <- med_lengths
+centroids$mean <- mean_lengths
+centroids$max <- max_lengths
 
 #Visual inspection
 mapview(Trees) +
   mapview(centroids, col.regions = "green") 
 
+
+# # Loop over all available tree shapes
+# for(i in 1:TotalTrees){
+#   
+#   # Convert polygon into points
+#   PolPoints <- st_cast(Trees[i,],"POINT")
+#   
+#   # Create rotated line object
+#   combined_lines <- st_sfc(geometry = st_sfc()) %>% st_set_crs(st_crs(centroids))
+#   
+#   # Create a new vector to store lengths
+#   Lengthvector <- vector()
+#   
+#   # Preclaculate centroid coordinate
+#   CentroidCoordX <- st_coordinates(centroids[i,])[1]
+#   CentroidCoordY <- st_coordinates(centroids[i,])[2]
+#   
+#   # Loop over all poligon points to create the lines between centroid and polygon edges
+#   for(j in 1:length(PolPoints[[1]])){
+#     
+#     # Define a set of coordinates to create line from the two points
+#     coordinates <- matrix(c(
+#       c(st_coordinates(PolPoints[j,])[1], CentroidCoordX),
+#       c(st_coordinates(PolPoints[j,])[2], CentroidCoordY)
+#     ), ncol = 2)
+#     
+#     # Create an sf LineString object
+#     line <- st_linestring(coordinates) %>% st_sfc() %>% st_set_crs(st_crs(centroids))
+#     
+#     # Calculate length of line
+#     Length <- st_length(line)
+#     
+#     # Combine the rotated LINESTRING objects into a single sfc object
+#     combined_lines <- st_combine(c(combined_lines, line))
+#     
+#     # Add up the lengths in the vector
+#     Lengthvector <- c(Lengthvector,c(Length))
+#     
+#   }
+#   
+#   # Calculate fields from the lines and store them on the respective centroid 
+#   centroids$min[i] <- round(min(Lengthvector),2)
+#   centroids$med[i] <- round(median(Lengthvector),2)
+#   centroids$mean[i] <- round(mean(Lengthvector),2)
+#   centroids$max[i] <- round(max(Lengthvector),2)
+#   
+#   # Increment progress bar
+#   setTxtProgressBar(pb, i)
+#   
+#   # Close progress bar when done
+#   if(i == TotalTrees){
+#     close(pb)
+#   }
+#   
+# }
 
 ###############################################################################
 ###############################################################################
